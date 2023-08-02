1dee7fed6c24cc66de5a57eaaaadb3ec4eaa07b4
Akrem Muktar <akremmuktar332@gmail.com>
2023-08-02 13:42:37 +0300

    fix

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -10,22 +10,25 @@ import PropTypes from 'prop-types';
 
 class App extends Component {
 
-  listCourses = [
+  static listCourses = [
     {id: 1, name: 'ES6', credit: 60},
     {id: 2, name: 'Webpack', credit: 20},
     {id: 3, name: 'React', credit: 40}
   ];
 
-  listNotifications = [
+  static listNotifications = [
     {id: 1, value: "New course available", type: "default"},
     {id: 2, value: "New resume available", type: "urgent"},
     {id: 3, html: {__html: getLatestNotification()}, type: "urgent"},
   ];
 
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    const  { isLoggedIn } = this.props;
   return (
-    <>
+    <React.Fragment>
       <div className='root-notifications'>
         <Notifications listNotifications={this.listNotifications}/>
       </div>
@@ -37,14 +40,14 @@ class App extends Component {
         {/* Body */}
         <div className="App-body">
           {/* Login */}
-          {isLoggedIn ? <CourseList listCourses={listCourses}/> : <Login />}
+          {this.props.isLoggedIn ? <CourseList listCourses={listCourses}/> : <Login />}
         </div>
         <div className='App-footer'>
           {/* Footer */}
           <Footer />
         </div>
       </div>
-    </>
+    </React.Fragment>
   );
   }
   
