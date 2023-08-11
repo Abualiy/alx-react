b5e3fd524dab6f49ffb368de3ceb239ecf78c47f
Akrem Muktar <akremmuktar332@gmail.com>
2023-08-11 14:33:26 +0300

    task_1

---
+++ 0x05-react_state/task_1/dashboard/src/Header/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from '../assets/holberton-logo.jpg';
+// import './Header.css';
+import { StyleSheet, css } from 'aphrodite';
+
+function Header() {
+    return (
+        <div className={css(styles.header)}>
+            <img className={css(styles.img)} src={logo} alt="Hoblerton-logo" />
+            <h1>School dashboard</h1>
+        </div>
+    );
+}
+
+const styles = StyleSheet.create({
+    header: {
+        width: '100%',
+        fontSize: '1.4rem',
+        color: '#e0354b',
+        display: 'flex',
+        alignItems: 'center',
+        borderBottom: '3px solid #e0354b'
+    },
+    img: {
+        width: '200px',
+        height: '200px'
+    }
+})
+
+export default Header;
